0f9152299b3a4dbaa1d5c1492e748ff406bd622d
Hiroaki Koguchi <kogu0507@gmail.com>
2025-01-26 17:10:01 +0900

    fullscreen module

---
+++ static/js/fullscreen.js
@@ -0,0 +1,23 @@
+// フルスクリーンの切り替えを設定する関数
+export function setupFullscreenToggle(buttonId) {
+    // 指定されたIDのボタンを取得
+    const fullscreenButton = document.getElementById(buttonId);
+    
+    if (!fullscreenButton) {
+        console.error(`Button with id '${buttonId}' not found.`);
+        return; // ボタンが見つからない場合は処理を中断
+    }
+
+    // フルスクリーンボタンのテキスト
+    const FULLSCREEN_TEXT = '🖥️ フルスクリーン';
+    const FULLSCREEN_EXIT_TEXT = '📴 フルスクリーン解除';
+
+    let isFullscreenMode = false;
+
+    // フルスクリーン切り替え処理
+    fullscreenButton.addEventListener('click', () => {
+        document.body.classList.toggle('fullscreen-mode');
+        isFullscreenMode = !isFullscreenMode;
+        fullscreenButton.textContent = isFullscreenMode ? FULLSCREEN_EXIT_TEXT : FULLSCREEN_TEXT;
+    });
+}
